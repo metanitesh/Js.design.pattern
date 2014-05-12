7b8ca9fd7c1072e7d66441e58bd43901dd25025b
nitesh sharma <nitesh.grep@gmail.com>
2014-05-12 22:46:57 +0530

    caching hasOwn to avoid long lookup

--- for_in_loop.js
+++ for_in_loop.js
@@ -16,7 +16,8 @@ for (prop in car){
 	/*checking hasOwnProperty would discard unwanted properties 
 	form prototype chain and using it by calling Objects hasOwnProperty 
 	avoid make it jerkproof */
-	if(Object.prototype.hasOwnProperty.call(car, prop)){
-		console.log(prop)
+	hasOwn = Object.prototype.hasOwnProperty;
+	if(hasOwn.call(car, prop)){
+		console.log(prop);
 	}
 }
