f7a5146a2c24d39fd2c7d222a2140cdd7a212f2a
nitesh sharma <nitesh.grep@gmail.com>
2014-05-13 16:22:30 +0530

    immediate object initialization

---
+++ immediate_object_initialization.js
@@ -0,0 +1,10 @@
+({
+	maxWidth : 600,
+	maxHeight : 800,
+	gimmeMax : function(){
+		return this.maxWidth + "x" + this.maxHeight
+	},
+	init : function(){
+		console.log(this.gimmeMax());
+	}
+}).init();
