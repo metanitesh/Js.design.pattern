73bcdd83f37c6d0d00222a1e19fa0cfbaf747da6
nitesh sharma <nitesh.grep@gmail.com>
2014-05-13 10:25:09 +0530

    enforcing new

---
+++ enforcing_new.js
@@ -0,0 +1,21 @@
+var Car = function(){
+	/* we can also use argument.callee 
+	instead of constructor name */  
+	if(!(this instanceof Car)){
+		return new Car(); 
+	}
+	this.name = "tesla_s"
+	this.type = "electric";
+}
+
+Car.prototype.getName = function(){
+	return this.name;
+}
+
+
+var car = Car(); /* it works even if we forget to use new*/
+var car2 = new Car();
+
+
+console.log(car.getName())
+console.log(car2.getName())
