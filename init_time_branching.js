15715669dd765a74167d4a9eed143072cc3cfb42
nitesh sharma <nitesh.grep@gmail.com>
2014-05-13 16:55:04 +0530

    init time branching for avoiding unnecessary rechecking

---
+++ init_time_branching.js
@@ -0,0 +1,17 @@
+var util = {
+	addListener: null,
+	removeListener: null
+};
+
+if(typeof window.addEventListener === "function"){
+	util.addListener = function(el, type, fn){
+		el.addEventListener(type, fn, false);
+	};
+}else if(typeof document.attachEvent === "function"{
+	util.addListener = function(el, type, fn){
+		el.attachEvent("on"+type, fn);
+	}
+})else {
+	utils.addListener = function (el, type, fn) {
+		el['on' + type] = fn; };
+}
