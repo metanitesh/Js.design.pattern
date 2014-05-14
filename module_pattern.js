5a9096039d701f61d9c22be7bb343631cc018812
nitesh sharma <nitesh.grep@gmail.com>
2014-05-14 13:12:54 +0530

    module pattern

---
+++ module_pattern.js
@@ -0,0 +1,33 @@
+//using namespace pattern 
+MYAPP.namespace("MYAPP.utilities.array");
+
+
+MYAPP.utilities.array = (function(){
+	
+	//dependencies declaration patter
+	var uobj = MYAPP.utilities.object;
+	var ulang = MYAPP.utilities.lang;
+
+	//private properties
+	var array_string = "[object Array]";
+	var ops = Object.prototype.toString;
+	
+	//private method 
+	var isArray = function(){
+
+	};
+
+	//one time initialization (optional)
+
+	//public api
+	return {
+		inArray : function(){
+			//logic
+		},
+
+		indexOf : function(){
+			
+		}
+	};
+
+}());
