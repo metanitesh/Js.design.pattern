6e95b023da9565a80a51180c4beaa75f02c6de0f
nitesh sharma <nitesh.grep@gmail.com>
2014-05-14 18:42:48 +0530

    chaining pattern

---
+++ chaining.js
@@ -0,0 +1,16 @@
+var obj = {
+	value : 1,
+	increment : function(){
+		this.value ++;
+		return this;
+	},
+	add : function(v){
+		this.value += v;
+		return this;
+	},
+	shout : function(){
+		console.log(this.value);
+	}
+};
+
+obj.increment().add(2).shout() //4
