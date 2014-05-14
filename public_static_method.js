7412c40f65b6f68553e9affd1987120ac89476b0
nitesh sharma <nitesh.grep@gmail.com>
2014-05-14 17:52:33 +0530

    public static method

---
+++ public_static_method.js
@@ -0,0 +1,13 @@
+var Gadgets = function(){
+};
+
+//static method
+Gadgets.isShiny = function(){
+	//this will point to constructor here
+	return "you bet";
+};
+
+Gadgets.prototype.setPrice = function(price){
+	this.price = price;
+};
+
