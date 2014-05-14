2b960f5f08fe3bd5969abc61b9b7f03c446e99c4
nitesh sharma <nitesh.grep@gmail.com>
2014-05-14 10:31:55 +0530

    curry or partial application

---
+++ curry.js
@@ -0,0 +1,16 @@
+var curryMe = function (fn) {
+	var slice = Array.prototype.slice;
+	var stores_args = slice.call(arguments, 1);
+	return function(){
+		var new_args = slice.call(arguments)
+		var args = stores_args.concat(new_args);
+		return fn.apply(null, args);
+	}
+}
+
+var add = function(x,y){
+	return x+y;
+}
+
+var newAdd = curryMe(add, 5);
+console.log(newAdd(4))//9
