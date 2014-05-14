9815968e81da62d6d2f65ff99c38b62a3d8217ca
nitesh sharma <nitesh.grep@gmail.com>
2014-05-14 11:17:16 +0530

    namespace pattern

---
+++ namespace.js
@@ -0,0 +1,20 @@
+var namespace = function(ns_string){
+	var parts = ns_string.split(".");
+	var parent = MYAPP = {};
+
+	if(parts[0] === "MYAPP"){
+		parts = parts.slice(1);
+	}
+
+	for(var i=0, max=parts.length; i<max; i++){
+		if(typeof parent[parts[i]] === "undefined"){
+			parent[parts[i]] = {};
+		}
+		parent = parent[parts[i]]
+	}
+
+	return parent;
+}
+
+namespace("MYAPP.module1.model");
+console.log(MYAPP.module1.model) // {};
