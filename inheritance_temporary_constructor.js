7ca7323a842338178436e0ce45ea5fdd033af068
nitesh sharma <nitesh.grep@gmail.com>
2014-05-15 21:02:11 +0530

    temporary constructor inheritance

---
+++ inheritance_temporary_constructor.js
@@ -0,0 +1,24 @@
+var inherit = function(C,P){
+	var F = function(){};
+	F.prototype = P.prototype;
+	C.prototype = new F();
+	C.uber  = P.prototype;
+	C.prototype.constructor = C;
+};
+
+var Animal = function(){
+	this.name = "Animal";
+};
+
+Animal.prototype.say = function(){
+	console.log(this.name);
+};
+
+var Person = function(){
+	this.name = "person";
+};
+
+inherit(Person, Animal);
+
+var person = new Person();
+console.log(person);
