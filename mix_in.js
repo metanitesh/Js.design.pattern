02373d14bed1dbdd967c6dddc696543640a033b4
nitesh sharma <nitesh.grep@gmail.com>
2014-05-16 19:21:08 +0530

    mix in pattern for copying from multiple object

---
+++ mix_in.js
@@ -0,0 +1,21 @@
+var mix = function(){
+	var child = {};
+
+	for(var i=0, max=arguments.length; i<max; i++){
+		for(var prop in arguments[i]){
+			if(arguments[i].hasOwnProperty(prop)){
+				child[prop] = arguments[i][prop];
+			}
+		}
+	}
+	return child;
+};
+
+var cake = mix(
+	{eggs: 3, large: true},
+	{butter: 1, salted: true},
+	{sugar: "sure"}
+);
+
+console.log(cake);
+
