0efde01fedeb06cae0f645a1c6a797d1b19df6ce
nitesh sharma <nitesh.grep@gmail.com>
2014-05-16 12:53:58 +0530

    prototypal inheritance

---
+++ inheritance_modern.js
@@ -0,0 +1,28 @@
+/*or use Object.create*/
+var object = function(o){
+	var F = function(){};
+	F.prototype = o;
+	return new F();
+};
+
+var parent = {
+	lastName : "sharma",
+	getLastName : function(){
+		return this.lastName;
+	}
+};
+
+var Teacher = function(){
+	this.name = "Adam";
+};
+
+Teacher.prototype.taught = function(){
+	console.log("maths");
+};
+
+
+var kid = object(parent);
+var kid2 = object(Teacher.prototype);
+
+console.log(kid.getLastName());
+console.log(kid2.taught());
