0a0a92abe02fa8b884ee00c3c36d5f1beb65ed70
nitesh sharma <nitesh.grep@gmail.com>
2014-05-16 12:36:45 +0530

    inheritance by mimicking class using klass

---
+++ inheritance_klass.js
@@ -0,0 +1,49 @@
+var klass = function(Parent, props) {
+	var Child;
+
+	Child = function() {
+		if (Child.uber && Child.uber.hasOwnProperty("__constructor")) {
+			Child.uber.__constructor.apply(this, arguments);
+		}
+
+		if (Child.prototype.hasOwnProperty("__constructor")) {
+			Child.prototype.__constructor.apply(this, arguments);
+		}
+
+	};
+	Parent = Parent || Object;
+
+	var F = function() {};
+	F.prototype = Parent.prototype;
+	Child.prototype = new F();
+	Child.uber = Parent.prototype;
+	Child.prototype.constructor = Child;
+
+	for (var i in props) {
+		if (props.hasOwnProperty(i)) {
+			Child.prototype[i] = props[i];
+		}
+	}
+
+
+
+	return Child;
+};
+
+
+var Man = klass(null, {
+	__constructor: function(name) {
+		console.log("man");
+		this.name = name;
+	},
+	getName: function() {
+		console.log(this.name);
+	}
+});
+
+var SuperMan = klass(Man, {
+	__constructor: function(name) {
+		console.log("SuperMan");
+		this.name = name;
+	}
+});
