454e6720dafbd8728763b3d9648d83c97b365a40
nitesh sharma <nitesh.grep@gmail.com>
2014-05-17 10:18:14 +0530

    singleton using static and immediate function

---
+++ singleton.js
@@ -0,0 +1,38 @@
+/*using static property*/
+var Universe = function(){
+	if(typeof Universe.instance === "object"){
+		return Universe.instance;
+	}
+
+	this.startTime = 0;
+	this.bang = "big";
+
+	Universe.instance = this;
+	return this;
+}
+
+
+/*using immediate function */
+var Universe;
+
+(function(){
+	var instance;
+
+	Universe = function(){
+		if(instance){
+			return instance;
+		}
+
+		instance = this;
+
+		this.startTime = 0;
+		this.bang = "big";
+	};
+
+}());
+
+
+var uni = new Universe();
+var uni2 = new Universe();
+
+console.log(uni === uni2)
