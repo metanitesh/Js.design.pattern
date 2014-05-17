81221aa7d1f572cf07256a9371b4f7ffac071353
nitesh sharma <nitesh.grep@gmail.com>
2014-05-17 11:55:12 +0530

    iterator pattern

---
+++ iterator.js
@@ -0,0 +1,31 @@
+var agg = (function(){
+	var index = 0;
+	var data = [1,2,3,4,5,6];
+	var length = data.length;
+
+	return {
+		next: function(){
+			var element;
+			if(!this.hasNext()){
+				return null;
+			}
+
+			element = data[index];
+			index = index + 2;
+			return element;
+
+		},
+		hasNext: function(){
+			return index<length;
+		},
+		rewind: function(){
+			index = 0;
+		},
+		current: function(){
+			return data[index];
+		}
+	};
+}());
+
+console.log(agg.next());
+console.log(agg.next());
