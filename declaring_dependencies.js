00647bcab0b241d7e13eab68261207789f0173e6
nitesh sharma <nitesh.grep@gmail.com>
2014-05-19 19:45:28 +0530

    dependencies declartion pattern

---
+++ declaring_dependencies.js
@@ -0,0 +1,9 @@
+/*this is quite simple one but much neat 
+than passing around the objects in constructor*/
+
+var myAwesomeFeature = function(){
+	
+	var event = Yahoo.util.Event	
+	var dom = Yahoo.util.Dom	
+
+}
