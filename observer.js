cb986afee7103f783d78adf378a17e3fa34cc0b4
nitesh sharma <nitesh.grep@gmail.com>
2014-05-20 19:15:59 +0530

    observer pattern

---
+++ observer.js
@@ -0,0 +1,41 @@
+var Publisher = {
+	subscirbers: {
+		any: []
+	},
+
+	subscribe: function(fn, type) {
+
+		type = type || "any";
+		if (typeof this.subscirbers[type] === "undefined") {
+			this.subscirbers[type] = [];
+		}
+
+		this.subscirbers[type].push(fn);
+	},
+
+	unsubscribe: function(fn, type) {
+		if (typeof this.subscirbers[type] !== undefined) {
+			this.subscirbers[type].pull(fn);
+		}
+	},
+
+	publish: function(type, obj) {
+		var subscribers = this.subscirbers[type];
+
+		for (var i = 0, max = subscribers.length; i < max; i++) {
+			subscribers[i](obj);
+		}
+	}
+};
+
+
+var paper = Object.create(Publisher);
+var joe = {
+	shout : function(obj){
+		console.log("oh i got issue" + obj);
+	}
+};
+
+paper.subscribe(joe.shout, "new issue");
+paper.publish("new issue", 27);
+
