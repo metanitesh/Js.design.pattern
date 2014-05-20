d1efaccae6710ef00da1d74e84f7c3139fbc3287
nitesh sharma <nitesh.grep@gmail.com>
2014-05-20 07:50:20 +0530

    mediator pattern

---
+++ mediator.js
@@ -0,0 +1,61 @@
+var Player = function(name){
+	this.points = 0;
+	this.name = name ;
+};
+
+Player.prototype.play = function(){
+	this.points += 1;
+	mediator.played();
+};
+
+var scoreboard = {
+	element : $("#result"),
+
+	update: function(score){
+		var i;
+		var msg = "";
+		for (i in score){
+			if(score.hasOwnProperty(i)){
+				msg += i;
+				msg += score[i];
+			}
+		}
+	}
+};
+
+var mediator = {
+	players : {},
+
+	setup : function(){
+		var players = this.players;
+		players.home = new Player("Home");
+		players.guest = new Player("Guest")
+
+	},
+
+	played: function(){
+		var players = this.players;
+		var score = {
+			Home: players.home.points,
+			Guest : players.guest.points
+		};
+
+		scoreboard.update(score);
+	},
+
+	keypress: function(e){
+		if(e.which === 49){
+			mediator.players.home.play();
+			return;
+		}
+
+		if(e.which === 48){
+			mediator.players.guest.play();
+			return;
+		}
+	}
+};
+
+
+mediator.setup();
+window.onkeypress = mediator.keypress;
