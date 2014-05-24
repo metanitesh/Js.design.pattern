addc025476f49615ba82a2671021496e6dd7daff
nitesh sharma <nitesh.grep@gmail.com>
2014-05-24 11:30:21 +0530

    prototypal inheritance advance

---
+++ inheritance_prototypal_advance.js
@@ -0,0 +1,27 @@
+var Model = {
+	
+	prototype: {
+		init: function() {
+			console.log("instantiate user");
+		}
+	},
+	create: function() {
+		var object = Object.create(this);
+		object.parent = this;
+		return object;
+	},
+
+	init: function() {
+		var instance = Object.create(this.prototype);
+		instance.parent = this;
+		instance.init.apply(instance,arguments);
+		return instance;
+	}
+
+};
+
+var User = Model.create();
+console.log(User);
+var user = User.init();
+console.log(user);
+
