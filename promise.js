17de703258f742160fa518de443594bd1988b3ee
nitesh sharma <nitesh.grep@gmail.com>
2014-09-16 11:12:13 +0530

    simple promises api

---
+++ promise.js
@@ -0,0 +1,34 @@
+var Promise = function(){
+
+    var done = [];
+    var fail = [];
+
+    this.done = function(fn){
+        done.push(fn);
+        return this;
+    }
+
+    this.failed = function(fn){
+        fail.push(fn);
+        return this;
+    }
+
+    this.resolve = function(result){
+        done.forEach(function(fn){
+            fn(result);
+        })
+        return this;
+    };
+
+    
+    this.fail =  function(){
+        fail.forEach(function(fn){
+            fn(result);
+        })
+        return this;
+    }
+}
+
+var promise  = new Promise();
+console.log(promise)
+
